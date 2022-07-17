0ffcc88daaab9908f6153adc084c4d9d122f0fb7
Susil Ramarao <susilramarao@Susils-MacBook-Air.local>
2022-07-17 22:46:38 +0530

    Initiated django project

--- webscrapper-test.py
+++ webscrapper-test.py
@@ -1,15 +1,45 @@
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 
-import requests
+#import requests
 
 
-URL = "https://takeuforward.org/interviews/strivers-sde-sheet-top-coding-interview-problems/"
-r = requests.get(URL)
+#URL = "https://takeuforward.org/interviews/strivers-sde-sheet-top-coding-interview-problems/"
+#r = requests.get(URL)
 #print(r.content)
 
-soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib
-print(soup.prettify())
+#soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib
+#print(soup.prettify())
 
+from typing import Set
+import requests
+from bs4 import BeautifulSoup
+import csv
+import re
+
+
+#URL = "http://w...content-available-to-author-only...s.com/inspirational-quotes"
+
+#URL = "https://t...content-available-to-author-only...d.org/interviews/strivers-sde-sheet-top-coding-interview-problems/"
+URL = "https://takeuforward.org/interviews/strivers-sde-sheet-top-coding-interview-problems/"
+r = requests.get(URL)
+
+soup = BeautifulSoup(r.content, 'html5lib')
+
+#quotes=[]  # a list to store quotes
+
+#table = soup.find('div', attrs = {'id':'all_quotes'})
+
+set = set()
+for link in soup.find_all('a'):
+    link_copy = re.search(".*(leetcode|geeksforgeeks).*", link.get('href'))
+    if link_copy != None:
+        temp = link_copy.string
+        if "problems" in temp:
+            temp = temp.split("problems")[1].split('/')[1]
+            set.add(temp)
+
+for i in set:
+    print(i)
 """
 
 TODOs:
